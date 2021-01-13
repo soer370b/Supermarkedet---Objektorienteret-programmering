bdbc421dd5acf334e4fd62d03f55b1db009cf2ad
soer370b <soer370b@edu.sde.dk>
2021-01-13 14:23:34 +0100

    Minor file edits

--- praesentationslag.py
+++ praesentationslag.py
@@ -5,45 +5,56 @@ from logiklag import *
 from datalag import *
 print("Import complete")
 
-#variable
-#knap størrelse
-height = 8
-width = 20
+def varegruppe():
+    print('Varegruppeside')
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.master.geometry('1080x720')
-        self.pack(fill = 'both', expand = True)
-        self.create_widgets()
+def varer():
+    print('Varer')
 
-    def create_widgets(self):
-        self.varegruppe = tk.Button(self)
-        self.varegruppe["text"] = "Varegrupper"
-        self.varegruppe["command"] = self.varegruupper
-        self.varegruppe['height'] = height
-        self.varegruppe['width'] = width
-        self.varegruppe.pack(side='top')
+class Main(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
 
-        self.vare = tk.Button(self)
-        self.vare["text"] = "Vare"
-        self.vare["command"] = self.varer
-        self.vare['height'] = height
-        self.vare['width'] = width
-        self.vare.pack(side='top')
+        self.logo = tk.Label(self, text="Logo")
+        self.buttons = []
+        for i in range(6):
+            self.buttons.append(tk.Button(self))
+        self.main = tk.Frame(self)
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit['height'] = height
-        self.quit['width'] = width
-        self.quit.pack(side='top')
+        self.logo.grid(row=0, column=0, rowspan=2, sticky="nsew")
+        self.varegruppe = self.buttons[0]
+        self.varegruppe["text"] = "Varegruppe"
+        self.varegruppe["command"] = self.varegrup
+        self.varer = self.buttons[1]
+        self.varer['text'] = 'Varer'
+        self.varer['command'] = self.vare
+        self.buttons[0].grid(row=2, column=0, sticky="nsew")
+        self.buttons[1].grid(row=3, column=0, sticky="nsew")
+        self.buttons[2].grid(row=4, column=0, sticky="nsew")
+        self.buttons[3].grid(row=5, column=0, sticky="nsew")
+        self.buttons[4].grid(row=6, column=0, sticky="nsew")
+        self.buttons[5].grid(row=7, column=0, sticky="nsew")
+        self.main.grid(row=2, column=2, columnspan=2, rowspan=6)
 
-    def varegruupper(self):
-        print("Varegrupper!")
+        for row in range(8):
+            self.grid_rowconfigure(row, weight=1)
+        for col in range(3):
+            self.grid_columnconfigure(col, weight=1)
 
-    def varer(self):
+        for row in range(8):
+            self.grid_rowconfigure(row, weight=1)
+        for col in range(3):
+            self.grid_columnconfigure(col, weight=1)
+
+    def varegrup(self):
+        print('Varegruppeside')
+
+    def vare(self):
         print('Varer')
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Main(root).pack(fill="both", expand=True)
+    root.geometry("1080x720")
+    root.mainloop()
