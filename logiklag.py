28945444afa7970f3f3fd89580ee124ed16713c1
soer370b <soer370b@edu.sde.dk>
2021-01-14 10:22:51 +0100

    Søren har lavet et halvt vindue

--- logiklag.py
+++ logiklag.py
@@ -1,22 +1,22 @@
 import datetime
 
 class Product():
-    def __init__(self):
+    def __init__(self, productgroup):
         self.productid = 0
         self.name = ''
         self.price = 0
-        self.productgroup = ''
+        self.productgroup = productgroup
         self.purchasedate = datetime.date()
         self.purchaseprice = 0
         self.location = ''
 
 class Productgroup():
     def __init__(self):
-        # self.id = 0
+        self.id = 0
         self.name = ''
 
-class ProductID(type):
-    def __init__(self):
+class ProductID():
+    def __init__(self, type):
         if type == 'EAN13':
             print('EAN13')
         if type == 'PLU':
