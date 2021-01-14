21335f8abbf8b7abcdb0a96198d6cad0d0631671
thom420e <thom420e@edu.sde.dk>
2021-01-14 11:08:54 +0100

    sletning af dublering

--- praesentationslag.py
+++ praesentationslag.py
@@ -5,55 +5,7 @@ from logiklag import *
 from datalag import *
 print("Import complete")
 
-def varegruppe():
-    print('Varegruppeside')
-
-def varer():
-    print('Varer')
-
-class ProductgroupWindow(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(self, parent)
-        tk.Frame.__init__(self, parent)
-
-        self.logo = tk.Label(self, text="Varegruppeside")
-        self.buttons = []
-        for i in range(6):
-            self.buttons.append(tk.Button(self))
-        self.main = tk.Frame(self)
-
-        self.logo.grid(row=0, column=0, rowspan=2, sticky="nsew")
-        self.varegruppe = self.buttons[0]
-        self.varegruppe["text"] = "Ny varegruppe"
-        self.varegruppe["command"] = self.varegrup
-        self.varer = self.buttons[1]
-        self.varer['text'] = 'Varer'
-        self.varer['command'] = self.vare
-        self.buttons[0].grid(row=2, column=0, sticky="nsew")
-        self.buttons[1].grid(row=3, column=0, sticky="nsew")
-        self.buttons[2].grid(row=4, column=0, sticky="nsew")
-        self.buttons[3].grid(row=5, column=0, sticky="nsew")
-        self.buttons[4].grid(row=6, column=0, sticky="nsew")
-        self.buttons[5].grid(row=7, column=0, sticky="nsew")
-        self.main.grid(row=2, column=2, columnspan=2, rowspan=6)
-
-        for row in range(8):
-            self.grid_rowconfigure(row, weight=1)
-        for col in range(3):
-            self.grid_columnconfigure(col, weight=1)
-
-        for row in range(8):
-            self.grid_rowconfigure(row, weight=1)
-        for col in range(3):
-            self.grid_columnconfigure(col, weight=1)
-
-    def varegrup(self):
-        print('Varegruppeside')
-
-    def vare(self):
-        print('Varer')
-
-
+############################Start############################
 class Main(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
