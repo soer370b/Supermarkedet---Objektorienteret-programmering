8d2f29fee8d812739e019f777a44212fc5d1f9e5
soer370b <soer370b@edu.sde.dk>
2021-01-19 11:14:02 +0100

    Productgroup funktioner færdige.

--- datalag.py
+++ datalag.py
@@ -1,70 +1,78 @@
 import sqlite3
-from logiklag import Product, Productgroup, ProductID
 
 print('Starter')
 con = sqlite3.connect('database.db')
 print('Database åbnet')
 
-try:
-    con.execute("""CREATE TABLE productgroups (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name STRING)""")
-    con.execute("""CREATE TABLE products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, productid INTEGER,
-        name STRING, price FLOAT, productgroup INTEGER,
-        purchaseprice FLOAT, location STRING)""")
-    print('Tabellen er oprettet')
-except Exception as e:
-    print('Tabellen er åben')
-    print(e)
 
-# def new_productgroup(name):
-#     c = con.cursor()
-#     c.execute('SELECT name FROM productgroups WHERE name = ? ', (name,))
-#     a = c.fetchone()
-#     if a[0] != name:
-#         Productgroup.name = name
-#         c = con.cursor()
-#         c.execute('INSERT INTO productgroups (name) VALUES (?)', (Productgroup.name,))
-#         con.commit()
+class Data:
+    def __init__(self):
 
-def new_productgroup(name):
-    Productgroup.name = name
-    c = con.cursor()
-    c.execute('INSERT INTO productgroups (name) VALUES (?)', (Productgroup.name,))
-    con.commit()
+        try:
+            con.execute("""CREATE TABLE productgroups (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name STRING)""")
+            con.execute("""CREATE TABLE products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, productid INTEGER,
+                name STRING, price FLOAT, productgroup INTEGER,
+                purchaseprice FLOAT, location STRING)""")
+            print('Tabellen er oprettet')
+        except Exception as e:
+            print('Tabellen er åben')
+            print(e)
 
-def new_product(name, productid, price, productgroup, purchaseprice, location):
-    Product.name = name
-    Product.productid = productid
-    Product.price = price
-    Product.productgroup = productgroup
-    # Product.purchasedate = purchasedate
-    Product.purchaseprice = purchaseprice
-    Product.location = location
+    # def new_productgroup(self, name):
+    #     c = con.cursor()
+    #     c.execute('SELECT name FROM productgroups WHERE name = ? ', (name,))
+    #     a = c.fetchone()
+    #     if a[0] != name:
+    #         Productgroup.name = name
+    #         c = con.cursor()
+    #         c.execute('INSERT INTO productgroups (name) VALUES (?)', (Productgroup.name,))
+    #         con.commit()
 
-    c = con.cursor()
-    c.execute('''INSERT INTO products (name, productid, price,
-                productgroup, purchaseprice,
-                location) VALUES (?, ?, ?, ?, ?, ?)''', (Product.name,
-                Product.productid, Product.price, Product.productgroup,
-                Product.purchaseprice, Product.location, ))
-    con.commit()
-#note Der mangler en datetime i varekolonnen.
+    def new_productgroup(self, name):
+        c = con.cursor()
+        c.execute('SELECT name FROM productgroups WHERE name = ? ', (name,))
+        a = c.fetchone()
+        if a == None:
+            c = con.cursor()
+            c.execute('INSERT INTO productgroups (name) VALUES (?)', (name,))
+            con.commit()
 
-# name = 'Mejeri'
-#
-# new_productgroup(name)
+    def edit_productgroup(self, name, newname):
+        c = con.cursor()
+        c.execute('UPDATE productgroups SET name = (?) WHERE name = (?)', (newname, name))
+        con.commit()
 
-pname = 'Ost'
-pid = 1235
-price = 50
-ppgroup = 3
-ppprice = 25
-plocation = 'Køledisk'
+    def delete_productgroupe(self, name):
+        c = con.cursor()
+        c.execute('DELETE FROM productgroups WHERE name = (?)', (name,))
+        con.commit()
 
-new_product(pname, pid, price, ppgroup, ppprice, plocation)
+    def new_product(self, name, productid, price, productgroup, purchaseprice, location):
+        Product.name = name
+        Product.productid = productid
+        Product.price = price
+        Product.productgroup = productgroup
+        # Product.purchasedate = purchasedate
+        Product.purchaseprice = purchaseprice
+        Product.location = location
 
-PLU = 'PLU'
+        c = con.cursor()
+        c.execute('''INSERT INTO products (name, productid, price,
+                    productgroup, purchaseprice,
+                    location) VALUES (?, ?, ?, ?, ?, ?)''', (Product.name,
+                    Product.productid, Product.price, Product.productgroup,
+                    Product.purchaseprice, Product.location, ))
+        con.commit()
+    #note Der mangler en datetime i varekolonnen.
 
-ProductID.PLU = PLU
+    # name = 'Mejeri'
+    #
+    # new_productgroup(name)
+
+if __name__ == "__main__":
+    data = Data()
+    name = 'None food'
+    data.new_productgroup(name)
