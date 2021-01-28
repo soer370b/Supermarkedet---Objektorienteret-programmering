817dfa1bc07ea913e5859d1fbb0ed71a4c5b38eb
soer370b <56199827+soer370b@users.noreply.github.com>
2021-01-28 11:15:18 +0100

    Datalag med korrekt syntaks

--- datalag.py
+++ datalag.py
@@ -1,92 +1,55 @@
 import sqlite3
-<<<<<<< HEAD
 from logiklag import Productgroup, Product
 import Data_stregkode_vare_base
-=======
-
->>>>>>> Thomas
-print('Starter')
-con = sqlite3.connect('database.db')
-print('Database åbnet')
-
 
 class Data:
     def __init__(self):
-
+        print('Starter')
+        self.con = sqlite3.connect('database.db')
+        print('Database åbnet')
         try:
-            con.execute("""CREATE TABLE productgroups (
+            self.con.execute("""CREATE TABLE productgroups (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name STRING)""")
-            con.execute("""CREATE TABLE products (
-<<<<<<< HEAD
+            self.con.execute("""CREATE TABLE products (
                 id INTEGER PRIMARY KEY AUTOINCREMENT, productid INTEGER, PLU INTEGER,
-=======
-                id INTEGER PRIMARY KEY AUTOINCREMENT, productid INTEGER,
->>>>>>> Thomas
                 name STRING, price FLOAT, productgroup INTEGER,
                 purchaseprice FLOAT, location STRING)""")
             print('Tabellen er oprettet')
         except Exception as e:
             print('Tabellen er åben')
             print(e)
+        self.con.commit()
 
-<<<<<<< HEAD
-=======
-    # def new_productgroup(self, name):
-    #     c = con.cursor()
-    #     c.execute('SELECT name FROM productgroups WHERE name = ? ', (name,))
-    #     a = c.fetchone()
-    #     if a[0] != name:
-    #         Productgroup.name = name
-    #         c = con.cursor()
-    #         c.execute('INSERT INTO productgroups (name) VALUES (?)', (Productgroup.name,))
-    #         con.commit()
-
->>>>>>> Thomas
     def new_productgroup(self, name):
-        c = con.cursor()
+        c = self.con.cursor()
         c.execute('SELECT name FROM productgroups WHERE name = ? ', (name,))
         a = c.fetchone()
         if a == None:
-            c = con.cursor()
+            c = self.con.cursor()
             c.execute('INSERT INTO productgroups (name) VALUES (?)', (name,))
-            con.commit()
+            self.con.commit()
 
-<<<<<<< HEAD
     def edit_productgroup(self, id, newname):
-        c = con.cursor()
+        c = self.con.cursor()
         c.execute('UPDATE productgroups SET name = (?) WHERE id = (?)', (newname, id))
-        con.commit()
+        self.con.commit()
 
     def delete_productgroup(self, id):
-        c = con.cursor()
+        c = self.con.cursor()
         c.execute('DELETE FROM productgroups WHERE id = (?)', (id,))
-=======
-    def edit_productgroup(self, name, newname):
-        c = con.cursor()
-        c.execute('UPDATE productgroups SET name = (?) WHERE name = (?)', (newname, name))
-        con.commit()
-
-    def delete_productgroupe(self, name):
-        c = con.cursor()
-        c.execute('DELETE FROM productgroups WHERE name = (?)', (name,))
->>>>>>> Thomas
-        con.commit()
+        self.con.commit()
 
     def get_productgroups(self):
         id = []
         name = []
-<<<<<<< HEAD
         productgroups = []
-=======
->>>>>>> Thomas
-        c = con.cursor()
+        c = self.con.cursor()
         c.execute('SELECT * FROM productgroups')
         data = c.fetchall()
         print('Der er ' + str(len(data)) + ' produktgrupper')
         for i in range(len(data)):
             pid = str(data[i][0])
-<<<<<<< HEAD
             print('Id = ' + pid)
             pname = str(data[i][1])
             print('Navn = ' + pname + '\n')
@@ -97,7 +60,7 @@ class Data:
     def new_product(self, name, productid, PLU, price, productgroup, purchaseprice, location):
         def productgroup_chek(productgroup):
             cheker = False
-            pg = Data.get_productgroups()
+            pg = self.get_productgroups()
             for i in pg:
                 if i.name == productgroup:
                     cheker = True
@@ -119,22 +82,22 @@ class Data:
 
         if productgroup_chek(productgroup) == True:
             if PLU == '':
-                c = con.cursor()
+                c = self.con.cursor()
                 c.execute('''INSERT INTO products (name, productid, price,
                             productgroup, purchaseprice,
                             location) VALUES (?, ?, ?, ?, ?, ?)''', (name,
                             productid, price, productgroup,
                             purchaseprice, location, ))
-                con.commit()
+                self.con.commit()
 
             else:
-                c = con.cursor()
+                c = self.con.cursor()
                 c.execute('''INSERT INTO products (name, productid, PLU, price,
                             productgroup, purchaseprice,
                             location) VALUES (?, ?, ?, ?, ?, ?, ?)''', (name,
                             productid, PLU, price, productgroup,
                             purchaseprice, location, ))
-                con.commit()
+                self.con.commit()
         else:
             print('Produktgruppen findes ikke!')
 
@@ -158,37 +121,3 @@ if __name__ == "__main__":
     for p in a:
         print(p.id, p.name)
 '''
-=======
-            # print('Id = ' + pid)
-            pname = str(data[i][1])
-            # print('Navn = ' + pname + '\n')
-            id.append(pid)
-            name.append(pname)
-        list = [id, name]
-        return list
-
-    def new_product(self, name, productid, price, productgroup, purchaseprice, location):
-        Product.name = name
-        Product.productid = productid
-        Product.price = price
-        Product.productgroup = productgroup
-        # Product.purchasedate = purchasedate
-        Product.purchaseprice = purchaseprice
-        Product.location = location
-
-        c = con.cursor()
-        c.execute('''INSERT INTO products (name, productid, price,
-                    productgroup, purchaseprice,
-                    location) VALUES (?, ?, ?, ?, ?, ?)''', (Product.name,
-                    Product.productid, Product.price, Product.productgroup,
-                    Product.purchaseprice, Product.location, ))
-        con.commit()
-    #note Der mangler en datetime i varekolonnen.
-
-
-if __name__ == "__main__":
-    data = Data()
-    name = 'Fisk'
-    data.new_productgroup(name)
-    data.get_productgroups()
->>>>>>> Thomas
